a494e82c9797d5e43f8122e08fd250bfa5033050
陈政 <1427270056@qq.com>
2020-05-15 22:59:17 +0800

    完成了组件创建和路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,24 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+// import Cinema from '@/views/Cinema'
 
 Vue.use(VueRouter);
-
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    {path:"*/",redirect: "/movie"},
+      {path:"/cinema",component:()=>import("@/views/Cinema")},//路由按需加载
+      {path:"/mine",component:()=>import("@/views/Mine")},
+      {path:"/movie",component:()=>import("@/views/Movie"),redirect:"/movie/nowplaying",
+      children:[
+        {path:"city",component:()=>import("@/components/City")},
+        {path:"comingsoon",component:()=>import("@/components/Comingsoon")},
+        {path:"nowplaying",component:()=>import("@/components/Nowplaying")},
+        {path:"search",component:()=>import("@/components/Search")},
+      ]},
+      // {path:"/cinema",component:Cinema},
 ];
-
+Vue.component({
+  // Cinema,
+})
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="main">
+    <Header title="我的喵喵"/>
+    <Login/>
+    <Footer/>
+  </div>
+</template>
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import Login from '@/components/Login'
+export default {
+    name:"Mine",
+    components:{
+    Header,
+    Footer,
+    Login
+  }
+}
+</script>
+<style>
+
+</style>
