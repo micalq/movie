08bbe4ff0338a0f3d9d5958e5702c271131e7999
陈政 <1427270056@qq.com>
2020-05-16 14:28:36 +0800

    完成数据渲染

--- src/components/CinemaList/index.vue
+++ src/components/CinemaList/index.vue
@@ -1,88 +1,19 @@
 <template>
  	<div class="cinema_body">
 				<ul>
-					<li>
+					<li v-for="(item) in cinemaList" :key="item.id">
 						<div>
-							<span>大地影院(澳东世纪店)</span>
-							<span class="q"><span class="price">22.9</span> 元起</span>
+							<span>{{item.nm}}</span>
+							<span class="q"><span class="price">{{item.sellPrice}}</span> 元起</span>
 						</div>
 						<div class="address">
-							<span>金州区大连经济技术开发区澳东世纪3层</span>
-							<span>1763.5km</span>
+							<span>{{item.addr}}</span>
+							<span>{{item.distance}}</span>
 						</div>
 						<div class="card">
                 			<div>小吃</div>
                 			<div>折扣卡</div>
-       					</div>
-					</li>
-					<li>
-						<div>
-							<span>大地影院(澳东世纪店)</span>
-							<span class="q"><span class="price">22.9</span> 元起</span>
-						</div>
-						<div class="address">
-							<span>金州区大连经济技术开发区澳东世纪3层</span>
-							<span>1763.5km</span>
-						</div>
-						<div class="card">
-                			<div>小吃</div>
-                			<div>折扣卡</div>
-       					</div>
-					</li>
-					<li>
-						<div>
-							<span>大地影院(澳东世纪店)</span>
-							<span class="q"><span class="price">22.9</span> 元起</span>
-						</div>
-						<div class="address">
-							<span>金州区大连经济技术开发区澳东世纪3层</span>
-							<span>1763.5km</span>
-						</div>
-						<div class="card">
-                			<div>小吃</div>
-                			<div>折扣卡</div>
-       					</div>
-					</li>
-					<li>
-						<div>
-							<span>大地影院(澳东世纪店)</span>
-							<span class="q"><span class="price">22.9</span> 元起</span>
-						</div>
-						<div class="address">
-							<span>金州区大连经济技术开发区澳东世纪3层</span>
-							<span>1763.5km</span>
-						</div>
-						<div class="card">
-                			<div>小吃</div>
-                			<div>折扣卡</div>
-       					</div>
-					</li>
-					<li>
-						<div>
-							<span>大地影院(澳东世纪店)</span>
-							<span class="q"><span class="price">22.9</span> 元起</span>
-						</div>
-						<div class="address">
-							<span>金州区大连经济技术开发区澳东世纪3层</span>
-							<span>1763.5km</span>
-						</div>
-						<div class="card">
-                			<div>小吃</div>
-                			<div>折扣卡</div>
-       					</div>
-					</li>
-					<li>
-						<div>
-							<span>大地影院(澳东世纪店)</span>
-							<span class="q"><span class="price">22.9</span> 元起</span>
-						</div>
-						<div class="address">
-							<span>金州区大连经济技术开发区澳东世纪3层</span>
-							<span>1763.5km</span>
-						</div>
-						<div class="card">
-                			<div>小吃</div>
-                			<div>折扣卡</div>
+                      <!-- <div v-for="(item1,key) in item.tag" :key="key"><span v-if="item1===1"></span></div> -->
        					</div>
 					</li>
 				</ul>
@@ -90,7 +21,24 @@
 </template>
 <script>
 export default {
-
+        data() {
+          return {
+              cinemaList:[],
+          }
+        },
+        mounted() {
+            this.getCinemaList()
+        },
+        methods: {
+       async  getCinemaList(){
+          const {data:res}= await this.axios.get("/api/cinemaList?cityId=10");
+            // console.log(res);
+              if (res.msg=="ok"&&res.data.cinemas) {
+                this.cinemaList=res.data.cinemas;
+              }
+              // console.log(this.cinemaList)
+          }
+        },
 }
 </script>
 <style scoped>
--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -4,91 +4,105 @@
 					<div class="city_hot">
 						<h2>热门城市</h2>
 						<ul class="clearfix">
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
+							<li v-for="(item) in hotCity" :key="item.id">{{item.nm}}</li>
 						</ul>
 					</div>
-					<div class="city_sort">
-						<div>
-							<h2>A</h2>
+					<div class="city_sort" ref="toCity">
+						<div v-for="(item) in cityList" :key="item.index">
+							<h2>{{item.index}}</h2>
 							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
-							</ul>
-						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
-							</ul>
-						</div>
-						<div>
-							<h2>A</h2>
-							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
-							</ul>
-						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
-							</ul>
-						</div>
-						<div>
-							<h2>A</h2>
-							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
-							</ul>
-						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
+								<li v-for="(itemList) in item.list" :key="itemList.id">{{itemList.nm}}</li>
 							</ul>
 						</div>
 					</div>
 				</div>
 				<div class="city_index">
 					<ul>
-						<li>A</li>
-						<li>B</li>
-						<li>C</li>
-						<li>D</li>
-						<li>E</li>
+						<li v-for="(item,index) in cityList" :key="item.index" @touchstart="toCitylist(index)">{{item.index}}</li>
 					</ul>
 				</div>
+
 			</div>
 </template>
 <script>
 export default {
-
+    data() {
+      return {
+        cityList:[],//全部城市集合
+        hotCity:[],//热门城市集合
+      }
+    },
+    mounted() {
+        this.getCityList()
+    },
+    methods: {
+      getCityList(){
+        this.axios.get("/api/cityList").then((res)=>{
+          // console.log(res);
+          if (res.data.msg=="ok") {
+            let cities=res.data.data.cities;
+         const {cityList,hotCity}= this.formatCity(cities);
+            this.cityList=cityList;
+            this.hotCity=hotCity;
+            // console.log(cityList);
+          }
+        })
+      },
+      formatCity(cities){ //格式化城市
+        let cityList=[],hotCity=[];
+          for(let i=0,len=cities.length;i<len;i++){//热门城市
+                if (cities[i].isHot) {
+                  hotCity.push(cities[i])
+                }
+          }
+          console.log(hotCity)
+        for(let i=0,len=cities.length;i<len;i++){
+          let firstLetter=cities[i].py.substring(0,1).toUpperCase();//取出城市首字母进行比较
+            if   (toCom(firstLetter)) {
+              cityList.push({index:firstLetter,list:[{nm:cities[i].nm,id:cities[i].id}]})
+            } else {
+                for(let j=0,len=cityList.length;j<len;j++){
+                  if (cityList[j].index==firstLetter) {
+                    cityList[j].list.push({nm:cities[i].nm,id:cities[i].id})
+                  }
+                }
+            }
+        };
+        cityList.sort((a,b)=>{
+            if (a.index>b.index) {
+              return 1
+            } else if(a.index<b.index){
+              return -1
+            }else{
+              return 0
+            }
+        })
+        function toCom(firstLetter){
+        for(let i=0,len=cityList.length;i<len;i++){
+          if (cityList[i].index==firstLetter) {
+              return false
+          }
+        }
+        return true;
+      }
+      // console.log(cityList);
+      return{
+        cityList,hotCity
+      }
+      },
+      toCitylist(i){//跳转到城市
+      // console.log(i)
+      var h2=this.$refs.toCity.getElementsByTagName("h2");
+      // console.log(h2);
+      // console.log(this.$refs.toCity.parentNode);
+            this.$refs.toCity.parentNode.scrollTop = h2[i].offsetTop;
+            // console.log(1);
+      }
+    }
 }
 </script>
 <style >
-#content .city_body{ display: flex; width:100%;}
+#content .city_body{ display: flex; width:100%; position: relative; top: 0;bottom: 0;}
 .city_body .city_list{ flex:1; overflow: auto; background: #FFF5F0;}
 .city_body .city_list::-webkit-scrollbar{
     background-color:transparent;
@@ -101,5 +115,5 @@ export default {
 .city_body .city_sort h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;}
 .city_body .city_sort ul{ padding-left: 10px; margin-top: 10px;}
 .city_body .city_sort ul li{ line-height: 30px; line-height: 30px;}
-.city_body .city_index{ width:20px;  text-align: center; border-left:1px #e6e6e6 solid;margin-top: 50%;}
+.city_body .city_index{ width:20px;  text-align: center; border-left:1px #e6e6e6 solid;position: fixed; top:20% ;right:0;}
 </style>
